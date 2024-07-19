9bae72807d8bfa559080bd795bc17fe808968060
margie1a <162654709+margie1a@users.noreply.github.com>
2024-07-19 19:07:23 +0900

    Title: 살 수 있는 물건, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240719/살 수 있는 물건/things-able-to-buy.java
+++ 240719/살 수 있는 물건/things-able-to-buy.java
@@ -6,7 +6,7 @@ public class Main {
         int n = sc.nextInt();
         if (n >= 3000) {
             System.out.println("book");
-        } else if (n >= 1000) {
+        } else if (1000 <= n && n < 3000) {
             System.out.println("mask");
         }
     }
