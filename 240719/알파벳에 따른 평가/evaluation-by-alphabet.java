04b0fe68898612af10aa584c223dcc74609b453c
margie1a <162654709+margie1a@users.noreply.github.com>
2024-07-19 19:31:16 +0900

    Title: 알파벳에 따른 평가, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240719/알파벳에 따른 평가/evaluation-by-alphabet.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String n = sc.next();
+        if (n.equals("S")) {
+            System.out.println("Superior");
+        } else if (n.equals("A")) {
+            System.out.println("Excellent");
+        } else if (n.equals("B")) {
+            System.out.println("Good");
+        } else if (n.equals("C")) {
+            System.out.println("Usually");
+        } else if (n.equals("D")) {
+            System.out.println("Effort");
+        } else {
+            System.out.println("Failure");
+        }
+    }
+}
