1d63d1bf18fb6b8f3143922dfa0d7d480a36b072
margie1a <162654709+margie1a@users.noreply.github.com>
2024-07-20 16:50:14 +0900

    Title: 코로나 메뉴얼, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240720/코로나 메뉴얼/covid-manual.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String corona = sc.next();
+        int temp = sc.nextInt();
+        String a = "a";
+        if (corona.equals("Y")){
+            if (temp >= 37) {
+                a = "A";
+            }else{
+                a = "C";
+            }
+        }else {
+            if (temp >= 37) {
+                a = "B";
+            } else {
+                a = "D";
+            }
+        }
+        String corona_2 = sc.next();
+        int temp_2 = sc.nextInt();
+        String b = "a";
+        if (corona.equals("Y")){
+            if (temp >= 37) {
+                b = "A";
+            }else{
+                b = "C";
+            }
+        }else {
+            if (temp >= 37) {
+                b = "B";
+            } else {
+                b = "D";
+            }
+        }
+        String corona_3 = sc.next();
+        int temp_3 = sc.nextInt();
+        String c = "a";
+        if (corona.equals("Y")){
+            if (temp >= 37) {
+                c = "A";
+            }else{
+                c = "C";
+            }
+        }else {
+            if (temp >= 37) {
+                c = "B";
+            } else {
+                c = "D";
+            }
+        }
+        if ((a == "A" && b == "A") || (b == "A" && c == "A") || (a == "A" && c == "A")) {
+            System.out.println("E");
+        } else {
+            System.out.println("N");
+        }
+
+    }
+}
