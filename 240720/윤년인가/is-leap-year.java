35667947febc5e43d720008dbca6a4c305de516d
margie1a <162654709+margie1a@users.noreply.github.com>
2024-07-20 16:11:57 +0900

    Title: 윤년인가, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240720/윤년인가/is-leap-year.java
+++ 240720/윤년인가/is-leap-year.java
@@ -6,6 +6,8 @@ public class Main {
         int y = sc.nextInt();
         if(y % 4 == 0 && y % 100 != 0){
             System.out.println("true");
+        }else if(y % 100 == 0 && y % 400 != 0){
+            System.out.println("false");
         }else{
             System.out.println("false");
         }
