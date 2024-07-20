69b29a1813e32049b60133791161634674eac1a3
margie1a <162654709+margie1a@users.noreply.github.com>
2024-07-20 14:08:09 +0900

    Title: 시력 검사, Time: 136ms, Memory: 11MB, Status: Passed - CodeTree

--- 240720/시력 검사/eye-test.java
+++ 240720/시력 검사/eye-test.java
@@ -7,7 +7,7 @@ public class Main {
         double b = sc.nextDouble();
         if (a >= 1.0 && b >= 1.0) {
             System.out.println("High");
-        } else if (a <= 1.0 && a >= 0.5 & b <= 1.0 && b >= 0.5) {
+        } else if (a >= 0.5 && b >= 0.5) {
             System.out.println("Middle");
         } else {
             System.out.println("Low");
