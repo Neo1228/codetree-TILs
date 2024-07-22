ca92169a23bf06c39153df7d1cee59809fb1cfa4
margie1a <162654709+margie1a@users.noreply.github.com>
2024-07-23 02:57:07 +0900

    Title: 합성수 판별기, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

--- 240723/합성수 판별기/factorization-discriminator.java
+++ 240723/합성수 판별기/factorization-discriminator.java
@@ -8,11 +8,12 @@ public class Main {
         for (int i = 2; i <= (n - 1); i++) {
             if (n % i == 0) {
                 satisfied = true;
+                break;
             } else {
                 satisfied = false;
             }
         }
-        if (satisfied == true) {
+        if (satisfied) {
             System.out.println("C");
         } else {
             System.out.println("N");
