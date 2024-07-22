ca919a60c9181884c0bde9a65a8c9c9ae9ec188e
margie1a <162654709+margie1a@users.noreply.github.com>
2024-07-22 22:29:24 +0900

    Title: n번 더하기, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240722/n번 더하기/add-n-times.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int n = sc.nextInt();
+    for(int i = n; i > 0; i--){
+        a += n;
+        System.out.println(a);
+    }
+    }
+}
