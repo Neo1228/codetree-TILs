0bf057cf6447749ca07f9b964ac65aefe6b9a987
margie1a <162654709+margie1a@users.noreply.github.com>
2024-07-22 23:18:49 +0900

    Title: 369 게임, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240722/369 게임/369-game.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++){
+            String numStr = Integer.toString(i);
+            if(i % 3 == 0 || numStr.contains("3") || numStr.contains("6") || numStr.contains("9")){
+                System.out.print(0 + " ");
+            }else{
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
