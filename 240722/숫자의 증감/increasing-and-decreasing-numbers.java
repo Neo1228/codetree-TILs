a3799717163ad4192e75a466f79cf3b61f23486f
margie1a <162654709+margie1a@users.noreply.github.com>
2024-07-22 22:41:11 +0900

    Title: 숫자의 증감, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

--- 240722/숫자의 증감/increasing-and-decreasing-numbers.java
+++ 240722/숫자의 증감/increasing-and-decreasing-numbers.java
@@ -9,7 +9,7 @@ public class Main {
             System.out.print(i + " ");
         }
     }else{
-        for(int i = n; i >= 0; i--){
+        for(int i = n; i > 0; i--){
             System.out.print(i + " ");
         }
     }
