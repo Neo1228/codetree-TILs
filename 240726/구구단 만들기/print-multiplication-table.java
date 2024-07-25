b42660ab9db0cd026c31828a7be746aae1ae312c
margie1a <162654709+margie1a@users.noreply.github.com>
2024-07-26 00:18:05 +0900

    Title: 구구단 만들기, Time: 171ms, Memory: 10MB, Status: Passed - CodeTree

--- 240726/구구단 만들기/print-multiplication-table.java
+++ 240726/구구단 만들기/print-multiplication-table.java
@@ -7,7 +7,7 @@ public class Main {
         int b = sc.nextInt();
         for(int i = 1; i <= 9; i++){
             for (int j = b; j > 0; j--) {
-                if (j % 2 == 0) {
+                if (j % 2 == 0 && j >= a && j <= b) {
                     System.out.printf("%d * %d = %d", j, i, j * i);
                     if(a < 2){
                         if(j <= b && j > 2)
