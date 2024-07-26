6b4326c9a20971cc52fcea9f60b88e9001f7deaf
margie1a <162654709+margie1a@users.noreply.github.com>
2024-07-26 17:33:04 +0900

    Title: 3n + 1 수열 m번, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240726/3n + 1 수열 m번/3n-plus-1-sequence-m-times.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int cnt = 0;
+        for(int i = 1; i <= m; i++) {
+            int n = sc.nextInt();
+            while (n > 1) {
+                if (n % 2 == 0) {
+                    n = n / 2;
+                    cnt++;
+                    if (n == 1) {
+                        System.out.print(cnt);
+                    }
+                } else {
+                    n = n * 3 + 1;
+                    cnt++;
+                    if (n == 1) {
+                        System.out.print(cnt);
+                    }
+                }
+            }
+            System.out.println();
+
+        }
+    }
+}
