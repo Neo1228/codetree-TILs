06266d9c79983caa533a8758e7dff69e2adb4a0a
margie1a <162654709+margie1a@users.noreply.github.com>
2024-07-27 18:42:38 +0900

    Title: 배열의 특정 위치 찾기, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240727/배열의 특정 위치 찾기/find-specific-location-fo-array.java
+++ 240727/배열의 특정 위치 찾기/find-specific-location-fo-array.java
@@ -1,33 +1,22 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        // 입력을 받아서 배열에 저장합니다.
-        int[] numbers = new int[10];
+        int[] arr = new int[10];
+        int sum = 0;
+        int sum2 = 0;
+        int cnt = 0;
         for (int i = 0; i < 10; i++) {
-            numbers[i] = sc.nextInt();
+            arr[i] = sc.nextInt();
         }
-
-        // 짝수 번째로 입력된 값의 합을 계산합니다.
-        int evenIndexSum = 0;
-        for (int i = 1; i < 10; i += 2) { // 인덱스가 1부터 시작해서 2씩 증가
-            evenIndexSum += numbers[i];
+        for(int i = 0; i < 10; i++){
+            if(i % 2 == 1){
+                sum += arr[i];
+            }else if ((i + 1) % 3 == 0){
+                sum2 += arr[i];
+                cnt++;
+            }
         }
-
-        // 3의 배수 번째로 입력된 값의 평균을 계산합니다.
-        int multipleOfThreeSum = 0;
-        int count = 0;
-        for (int i = 2; i < 10; i += 3) { // 인덱스가 2부터 시작해서 3씩 증가
-            multipleOfThreeSum += numbers[i];
-            count++;
-        }
-        double multipleOfThreeAvg = (double) multipleOfThreeSum / count;
-
-        // 출력합니다.
-        System.out.printf("%d %.1f\n", evenIndexSum, Math.round(multipleOfThreeAvg * 10) / 10.0);
-
-        sc.close();
+        System.out.printf("%d %.1f", sum, (double)sum2 / cnt);
     }
 }
