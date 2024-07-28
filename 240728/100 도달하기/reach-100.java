b768687016210fbc2f029035986bd82e90038c1a
margie1a <162654709+margie1a@users.noreply.github.com>
2024-07-28 17:47:30 +0900

    Title: 100 도달하기, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240728/100 도달하기/reach-100.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+        int n = sc.nextInt();
+        arr[1] = n;
+        arr[0] = 1;
+        for(int i = 2; i < arr.length; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] > 100){
+                System.out.print(arr[i] + " ");
+                break;
+            }else{
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+}
