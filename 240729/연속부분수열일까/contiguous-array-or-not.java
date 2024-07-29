86de2fa702f30b117b08e50815e5ad9c08f10333
margie1a <162654709+margie1a@users.noreply.github.com>
2024-07-29 22:59:52 +0900

    Title: 연속부분수열일까, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

--- 240729/연속부분수열일까/contiguous-array-or-not.java
+++ 240729/연속부분수열일까/contiguous-array-or-not.java
@@ -3,28 +3,42 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // 입력 받기
         int n1 = sc.nextInt();
         int n2 = sc.nextInt();
-        int cnt = 0;
         int[] a = new int[n1];
         int[] b = new int[n2];
-        for(int i = 0; i < n1; i++) {
+        
+        for (int i = 0; i < n1; i++) {
             a[i] = sc.nextInt();
         }
-        for(int i = 0; i < n2; i++) {
+        
+        for (int i = 0; i < n2; i++) {
             b[i] = sc.nextInt();
         }
-        for (int i = 0; i < n1; i++) {
-            for(int j = 0; j < n2; j++){
-                if(a[i] == b[j]){
-                    cnt = i;
-                    if (a[i + 1] == b[j + 1]) {
-                        System.out.println("Yes");
-                    }else{
-                        System.out.println("No");
-                    }
+
+        // 연속 부분 수열인지 확인하는 로직
+        boolean found = false;
+        
+        for (int i = 0; i <= n1 - n2; i++) {
+            boolean isSubsequence = true;
+            for (int j = 0; j < n2; j++) {
+                if (a[i + j] != b[j]) {
+                    isSubsequence = false;
+                    break;
                 }
             }
+            if (isSubsequence) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
         }
     }
 }
