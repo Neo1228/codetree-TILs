c69f683f3f51f8e814ae53938457e0b2f6a2748a
margie1a <162654709+margie1a@users.noreply.github.com>
2024-07-30 02:17:12 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 193ms, Memory: 12MB, Status: Passed - CodeTree

--- 240730/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.java
+++ 240730/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.java
@@ -3,39 +3,32 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         int n = sc.nextInt();
-        int cnt = 0;
-        int[] arr = new int[n];
-        int[] count = new int[1001];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = sc.nextInt();
+        int[] prices = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            prices[i] = sc.nextInt();
         }
-        int benefit = 0;
-        for(int i = 0; i < arr.length; i++){
-            for(int j = i + 1; j < arr.length; j++){
-                if(arr[i] < arr[j]){
-                    benefit = arr[j] - arr[i];
-                    count[benefit]++;
-                    cnt++;
-                }
-            }
-        }
-        if(cnt == 0){
+        
+        if (n == 0) {
             System.out.println(0);
+            return;
         }
-        int maxCount = 0;
-        for(int i = 0; i < count.length; i++){
-            if(count[i] >= 1){
-                maxCount = i;
-                for(int j = 0; j < count.length; j++){
-                    if (count[j] >= 1 && maxCount > i) {
-                        maxCount = j;
-                    }
-                }
+        
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        
+        for (int i = 1; i < n; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else if (prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
             }
         }
-        if (maxCount != 0) {
-            System.out.println(maxCount);
-        }
+        
+        System.out.println(maxProfit);
+        
+        sc.close();
     }
 }
