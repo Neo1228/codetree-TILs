4924c923670b89b9e89a3aa2889a7bb699c32c3d
margie1a <162654709+margie1a@users.noreply.github.com>
2024-07-30 02:33:28 +0900

    Title: 두 숫자의 차의 최솟값, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 240730/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.java
+++ 240730/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.java
@@ -3,35 +3,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int cnt = 0;
-        int[] arr = new int[n];
-        int[] count = new int[100];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = sc.nextInt();
+
+        int n;
+        int[] A = new int[10];
+
+        n = sc.nextInt();
+        for(int i = 0; i < n; i++)
+            A[i] = sc.nextInt();
+
+        int minVal = A[1] - A[0];
+
+        for (int i = 2; i < n; i++){
+            if (minVal > A[i] - A[i - 1]){
+                minVal = A[i] - A[i - 1];
+            } 
         }
-        int benefit = 0;
-        for(int i = 0; i < arr.length; i++){
-            for(int j = i + 1; j < arr.length; j++){
-                if(arr[i] < arr[j]){
-                    benefit = arr[j] - arr[i];
-                    count[benefit]++;
-                    cnt++;
-                }
-            }
-        }
-        if(cnt == 0){
-            System.out.println(0);
-        }
-        int minCount = 0;
-        for(int i = 0; i < count.length; i++){
-            if(count[i] >= 1){
-                minCount = i;
-                break;
-            }
-        }
-        if (minCount != 0) {
-            System.out.println(minCount);
-        }
+
+        System.out.print(minVal);
     }
 }
