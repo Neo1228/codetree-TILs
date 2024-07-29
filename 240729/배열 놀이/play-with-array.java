0f18b7d2d2bd1ff95611b8ab5db17fb0306ab4e2
margie1a <162654709+margie1a@users.noreply.github.com>
2024-07-29 22:30:18 +0900

    Title: 배열 놀이, Time: 170ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240729/배열 놀이/play-with-array.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int q = sc.nextInt();
+        int b = 0;
+        int cnt = 0;
+        int[] arr = new int[n];
+        int[] question = new int[q];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < question.length; i++) {
+            int a = sc.nextInt();
+            if (a == 1) {
+                question[i] = sc.nextInt();
+                System.out.println(arr[question[i] - 1]);
+            } else if (a == 2) {
+                question[i] = sc.nextInt();
+                for (int j = 0; j < arr.length; j++) {
+                    if (arr[j] == question[i]) {
+                        cnt++;
+                        System.out.println(j + 1);
+                        continue;
+                    }
+                }
+                if(cnt < 1)
+                System.out.println(0);
+            }else if (a == 3) {
+                question[i] = sc.nextInt();
+                b = sc.nextInt();
+                for (int k = question[i] - 1; k <= b - 1; k++) {
+                    System.out.print(arr[k] + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
