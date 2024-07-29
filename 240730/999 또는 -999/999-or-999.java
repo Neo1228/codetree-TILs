ef6f165320a8ec98aaf4e444ef7d575f54961c33
margie1a <162654709+margie1a@users.noreply.github.com>
2024-07-30 00:10:29 +0900

    Title: 999 또는 -999, Time: 136ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240730/999 또는 -999/999-or-999.java
+++ 240730/999 또는 -999/999-or-999.java
@@ -3,18 +3,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
+        int[] arr = new int[100];
         int cnt = 0;
-        int maxVal = Integer.MIN_VALUE;
-        int minVal = Integer.MAX_VALUE;
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < 100; i++) {
             arr[i] = sc.nextInt();
             cnt++;
             if (arr[i] == 999 || arr[i] == -999) {
                 break;
             }
         }
+        int maxVal = arr[0];
+        int minVal = arr[0];
         for(int i = 0; i < cnt - 1; i++){
             if(arr[i] > maxVal){
                 maxVal = arr[i];
@@ -23,5 +22,6 @@ public class Main {
             }
         }
         System.out.println(maxVal + " " + minVal);
+
     }
 }
