b830962b44d719b34d65f0aceb120e6b069267fa
margie1a <162654709+margie1a@users.noreply.github.com>
2024-07-30 02:11:08 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240730/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.java
+++ 240730/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.java
@@ -6,7 +6,7 @@ public class Main {
         int n = sc.nextInt();
         int cnt = 0;
         int[] arr = new int[n];
-        int[] count = new int[n];
+        int[] count = new int[(int) Long.MAX_VALUE];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
         }
