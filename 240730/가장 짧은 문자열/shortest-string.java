c9d5cdf75c8124e3656ec7e046215e918d992ade
margie1a <162654709+margie1a@users.noreply.github.com>
2024-07-30 23:56:34 +0900

    Title: 가장 짧은 문자열, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

--- 240730/가장 짧은 문자열/shortest-string.java
+++ 240730/가장 짧은 문자열/shortest-string.java
@@ -6,17 +6,17 @@ public class Main {
         String str = sc.next();
         String str2 = sc.next();
         String str3 = sc.next();
-        if (str.length() > str2.length() && str.length() > str3.length() && str2.length() > str3.length()) {
+        if (str.length() > str2.length() && str.length() > str3.length() && str2.length() >= str3.length()) {
             System.out.println(str.length() - str3.length());
-        } else if (str.length() > str2.length() && str.length() > str3.length() && str3.length() > str2.length()){
+        } else if (str.length() > str2.length() && str.length() > str3.length() && str3.length() >= str2.length()){
             System.out.println(str.length() - str2.length());
-        } else if (str2.length() > str.length() && str2.length() > str3.length() && str3.length() > str.length()) {
+        } else if (str2.length() > str.length() && str2.length() > str3.length() && str3.length() >= str.length()) {
             System.out.println(str2.length() - str.length());
-        } else if (str2.length() > str.length() && str2.length() > str3.length() && str.length() > str3.length()) {
+        } else if (str2.length() > str.length() && str2.length() > str3.length() && str.length() >= str3.length()) {
             System.out.println(str2.length() - str3.length());
-        }else if(str3.length() > str2.length() && str3.length() > str.length() && str.length() > str2.length()) {
+        }else if(str3.length() > str2.length() && str3.length() > str.length() && str.length() >= str2.length()) {
             System.out.println(str3.length() - str2.length());
-        }else if(str3.length() > str2.length() && str3.length() > str.length() && str2.length() > str.length()) {
+        }else if(str3.length() > str2.length() && str3.length() > str.length() && str2.length() >= str.length()) {
             System.out.println(str3.length() - str.length());
         }
     }
