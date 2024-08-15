97d487002e3ce4dc2971cdb8cf71bc009e0d0f14
margie1a <162654709+margie1a@users.noreply.github.com>
2024-08-15 21:12:23 +0900

    Title: 그 요일은, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240815/그 요일은/the-day-of-the-day.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+        String day = sc.next();
+        int[] month_of_days = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String[] weeks = new String[]{"", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+        int elapsedWeek = 1;
+        int cnt = 0;
+        int idx = 0;
+        for(int i = 0; i < weeks.length; i++){
+            if(weeks[i].equals(day)){
+                idx = i;
+                break;
+            }
+        }
+
+        while (true) {
+            if (m1 == m2 && d1 == d2) {
+                break;
+            }
+            d1++;
+            elapsedWeek++;
+            if (d1 > month_of_days[m1]) {
+                d1 = 1;
+                m1++;
+            }
+            if(elapsedWeek == idx){
+                cnt++;
+            }
+            if (elapsedWeek == 7) {
+                elapsedWeek = 1;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
