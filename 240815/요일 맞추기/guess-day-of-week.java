3f0c5059e07fb9d088cc3e0464566e59cc80a3a7
margie1a <162654709+margie1a@users.noreply.github.com>
2024-08-15 21:01:07 +0900

    Title: 요일 맞추기, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

--- 240815/요일 맞추기/guess-day-of-week.java
+++ 240815/요일 맞추기/guess-day-of-week.java
@@ -7,7 +7,7 @@ public class Main {
         int d1 = sc.nextInt();
         int m2 = sc.nextInt();
         int d2 = sc.nextInt();
-        int[] month_of_days = new int[]{0, 31, 30, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int[] month_of_days = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         String[] weeks = new String[]{"", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
         int elapsedWeek = 2;
         if(m1 > m2 || (m1 == m2 && d1 > d2)){
@@ -34,12 +34,12 @@ public class Main {
             }
             d1++;
             elapsedWeek++;
-            if (d1 >= month_of_days[m1]) {
-                d1 = 0;
+            if (d1 > month_of_days[m1]) {
+                d1 = 1;
                 m1++;
             }
             if (weeks[elapsedWeek].equals(weeks[7])) {
-                elapsedWeek = 1;
+                elapsedWeek = 0;
             }
         }
         System.out.println(weeks[elapsedWeek]);
