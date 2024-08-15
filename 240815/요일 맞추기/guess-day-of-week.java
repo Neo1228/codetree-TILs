1d9a8a4bc22edbb87c86548454804a55764dbf06
margie1a <162654709+margie1a@users.noreply.github.com>
2024-08-15 20:53:59 +0900

    Title: 요일 맞추기, Time: 3082ms, Memory: 10MB, Status: Time Limit Exceed - CodeTree

---
+++ 240815/요일 맞추기/guess-day-of-week.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+        int elapsedTime = 0;
+        int[] month_of_days = new int[]{0, 31, 30, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String[] weeks = new String[]{"", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+        int elapsedWeek = 2;
+        if(m1 > m2 || (m1 == m2 && d1 > d2)){
+            while(true){
+                if(m1 == m2 && d1 == d2){
+                    break;
+                }
+                d1--;
+                elapsedTime++;
+                elapsedWeek--;
+                if(d1 == 0){
+                    m1--;
+                    d1 = month_of_days[m1];
+                }
+                if(weeks[elapsedWeek].equals(weeks[0])){
+                    elapsedWeek = 7;
+                }
+            }
+            System.out.println(weeks[elapsedWeek]);
+            return;
+        }
+        while (true) {
+            if (m1 == m2 && d1 == d2) {
+                break;
+            }
+            d1++;
+            elapsedTime++;
+            elapsedWeek++;
+            if (m1 > month_of_days[m1]) {
+                d1 = 0;
+                m1++;
+            }
+            if (weeks[elapsedWeek].equals(weeks[7])) {
+                elapsedWeek = 1;
+            }
+        }
+        System.out.println(weeks[elapsedWeek]);
+    }
+}
