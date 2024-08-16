dbb54a1d39d4352ccbaf20aa60db70464003febc
margie1a <162654709+margie1a@users.noreply.github.com>
2024-08-16 18:06:43 +0900

    Title: 최대로 겹치는 구간, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240816/최대로 겹치는 구간/maximum-overlapped-segments.java
+++ 240816/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -4,14 +4,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] arr = new int[200];
+        int[] arr = new int[300];
         for(int i = 0; i < n; i++){
             int a = sc.nextInt();
             int b = sc.nextInt();
             if(a < 0){
                 a += 100;
+            }else if(a > 0){
+                a += 100
             }
             if(b < 0){
+                b += 100;
+            }else if(b > 0){
                 b += 100;
             }
             for(int j = a; j <= b - 1; j++){
