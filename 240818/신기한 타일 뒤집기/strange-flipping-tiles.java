61d5b20e6712e6017de8d1857325dec6ea88569c
margie1a <162654709+margie1a@users.noreply.github.com>
2024-08-18 19:03:28 +0900

    Title: 신기한 타일 뒤집기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240818/신기한 타일 뒤집기/strange-flipping-tiles.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {
+    static final int MAX = 20;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] arr = new String[MAX];
+        int start = 10;
+
+        for (int i = 0; i < n; i++) {
+            int how = sc.nextInt();
+            String where = sc.next();
+            if (where.equals("R")) {
+                for (int j = start; j < start + how; j++) {
+                    arr[j] = "B";
+                }
+                start += how - 1;
+            } else if (where.equals("L")) {
+                for (int j = start; j >= start - how + 1; j--) {
+                    arr[j] = "W";
+                }
+                start -= how - 1;
+            }
+        }
+
+        int cntB = 0;
+        int cntW = 0;
+
+        for(int i = 0; i < MAX; i++) {
+            if(arr[i] != null){
+                if(arr[i].equals("W")) {
+                    cntW++;
+                }else if (arr[i].equals("B")){
+                    cntB++;
+                }
+            }
+        }
+        System.out.print(cntW + " " + cntB);
+
+    }
+}
