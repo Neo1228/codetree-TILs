9f5ea1587b2605a17fdcf36a8ced785f066240e3
margie1a <162654709+margie1a@users.noreply.github.com>
2024-08-18 19:03:42 +0900

    Title: 신기한 타일 뒤집기, Time: 221ms, Memory: 18MB, Status: Passed - CodeTree

--- 240818/신기한 타일 뒤집기/strange-flipping-tiles.java
+++ 240818/신기한 타일 뒤집기/strange-flipping-tiles.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
 public class Main {
-    static final int MAX = 20;
+    static final int MAX = 1000000;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         String[] arr = new String[MAX];
-        int start = 10;
+        int start = 500000;
 
         for (int i = 0; i < n; i++) {
             int how = sc.nextInt();
