83721de9ddd3fea36cc6bc3dff243a4b32e02fd3
margie1a <162654709+margie1a@users.noreply.github.com>
2024-08-19 23:28:49 +0900

    Title: 사각형의 총 넓이 2, Time: 476ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

--- 240819/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
+++ 240819/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
@@ -1,20 +1,21 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int MAX = 10000;
+    public static int MAX = 20000;
+    public static int HALF = MAX / 2;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int[][] arr = new int[MAX][MAX];
         for (int i = 0; i < N; i++){
             int row1 = sc.nextInt();
-            row1 += 100;
+            row1 += HALF;
             int col1 = sc.nextInt();
-            col1 += 100;
+            col1 += HALF;
             int row2 = sc.nextInt();
-            row2 += 100;
+            row2 += HALF;
             int col2 = sc.nextInt();
-            col2 += 100;
+            col2 += HALF;
             for(int j = row1; j < row2; j++){
                 for (int k = col1; k < col2; k++){
                     arr[j][k] = 1;
