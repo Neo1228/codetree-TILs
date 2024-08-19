3c9f05d289d9aeff092f4e302b276038aafbef1c
margie1a <162654709+margie1a@users.noreply.github.com>
2024-08-20 00:49:54 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240820/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
+++ 240820/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
@@ -1,80 +1,39 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int MAX = 2000;
-    public static int HALF = MAX / 2;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = 0;
-        int b = 0;
-        int c = 0;
-        int d = 0;
-        int[][] arr = new int[MAX][MAX];
-        for (int i = 0; i < 2; i++) {
-            int row1 = sc.nextInt();
-            row1 += HALF;
-            int col1 = sc.nextInt();
-            col1 += HALF;
-            int row2 = sc.nextInt();
-            row2 += HALF;
-            int col2 = sc.nextInt();
-            col2 += HALF;
-            if(i == 0) {
-                a = row1;
-                b = col1;
-                c = row2;
-                d = col2;
-            }
-            for (int j = row1; j < row2; j++) {
-                for (int k = col1; k < col2; k++) {
-                    arr[j][k] = 1;
-                }
-            }
-            if (i == 1) {
-                for (int j = row1; j < row2; j++) {
-                    for (int k = col1; k < col2; k++) {
-                        arr[j][k] = 2;
-                    }
-                }
-            }
-        }
 
-        int max = 0;
-        int cnt = 0;
-        int realMax = 0;
-        for(int i = a; i < c; i++) {
-            for(int j = b; j < d; j++) {
-                if(arr[i][j] == 1) {
-                    cnt++;
-                }
-                if(max < cnt){
-                    max = cnt;
-                }
-            }
-            if(realMax < max) {
-                realMax = max;
-            }
-            cnt = 0;
-        }
+        // 첫 번째 직사각형 좌표 입력
+        int x1_1 = sc.nextInt();
+        int y1_1 = sc.nextInt();
+        int x2_1 = sc.nextInt();
+        int y2_1 = sc.nextInt();
 
-        int cnt2 = 0;
-        int max2 = 0;
-        int realMax2 = 0;
-        for(int i = b; i < d; i++){
-            for(int j = a; j < c; j++) {
-                if(arr[j][i] == 1) {
-                    cnt2++;
-                }
-                if (max2 < cnt2) {
-                    max2 = cnt2;
-                }
-            }
-            if(realMax2 < max2) {
-                realMax2 = max2;
-            }
-            cnt2 = 0;
-        }
-        System.out.println(realMax * realMax2);
+        // 두 번째 직사각형 좌표 입력
+        int x1_2 = sc.nextInt();
+        int y1_2 = sc.nextInt();
+        int x2_2 = sc.nextInt();
+        int y2_2 = sc.nextInt();
 
+        // 겹치는 부분의 범위 계산
+        int overlapX1 = Math.max(x1_1, x1_2);
+        int overlapY1 = Math.max(y1_1, y1_2);
+        int overlapX2 = Math.min(x2_1, x2_2);
+        int overlapY2 = Math.min(y2_1, y2_2);
+
+        // 겹치지 않는 부분을 덮는 최소 사각형의 범위 계산
+        int remainingX1 = Math.min(x1_1, overlapX1);
+        int remainingY1 = Math.min(y1_1, overlapY1);
+        int remainingX2 = Math.max(x2_1, overlapX2);
+        int remainingY2 = Math.max(y2_1, overlapY2);
+
+        // 최소 직사각형의 넓이 계산
+        int width = remainingX2 - remainingX1;
+        int height = remainingY2 - remainingY1;
+        int area = width * height;
+
+        System.out.println(area);
     }
 }
