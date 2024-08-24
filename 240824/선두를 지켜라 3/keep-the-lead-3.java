d22c09f3697895a1f7c3d9b221925b7ecdfa96f3
margie1a <162654709+margie1a@users.noreply.github.com>
2024-08-24 16:59:41 +0900

    Title: 선두를 지켜라 3, Time: 242ms, Memory: 28MB, Status: Wrong Answer - CodeTree

--- 240824/선두를 지켜라 3/keep-the-lead-3.java
+++ 240824/선두를 지켜라 3/keep-the-lead-3.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int MAX = 100000;
+    public static int MAX = 1000000;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
