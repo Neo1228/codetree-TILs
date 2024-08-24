d8769dab7fff35e939411ceb352c9d6e840a86df
margie1a <162654709+margie1a@users.noreply.github.com>
2024-08-24 16:35:26 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 170ms, Memory: 11MB, Status: Wrong Answer - CodeTree

--- 240824/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.java
+++ 240824/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,43 +7,61 @@ public class Main {
         int k = sc.nextInt();
         int p = sc.nextInt();
         int T = sc.nextInt();
-        int[] infected = new int[n + 1];
-        int[] infectionTime = new int[n + 1];
-        int[][] shakes = new int[T][3];
-        infected[p] = 1;
-        infectionTime[p] = 0;
+        int[] infected = new int[n + 1]; // 이때 n+1은 n명을 처리하기 위한 배열
+        int[] time = new int[T];
+        int[] infectedX = new int[T];
+        int[] infectedY = new int[T];
+        infected[p] = 1; // 최초 감염자 설정
 
+        // 입력받은 값을 배열에 저장
         for (int i = 0; i < T; i++) {
-            shakes[i][0] = sc.nextInt(); // time
-            shakes[i][1] = sc.nextInt(); // x
-            shakes[i][2] = sc.nextInt(); // y
+            int t = sc.nextInt();
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            time[i] = t;
+            infectedX[i] = x;
+            infectedY[i] = y;
         }
 
         // 시간에 따른 이벤트 정렬
-        Arrays.sort(shakes, (a, b) -> Integer.compare(a[0], b[0]));
-
-        // 감염 확산 처리
-        for (int i = 0; i < T; i++) {
-            int t = shakes[i][0];
-            int x = shakes[i][1];
-            int y = shakes[i][2];
-            
-            if (infected[x] == 1 && t - infectionTime[x] <= k) {
-                if (infected[y] == 0) {
-                    infected[y] = 1;
-                    infectionTime[y] = t;
+        for(int i = 0; i < T; i++){
+            int minIndex = i;
+            for(int j = i + 1; j < T; j++){
+                if(time[j] < time[minIndex]){
+                    minIndex = j;
                 }
             }
-            if (infected[y] == 1 && t - infectionTime[y] <= k) {
-                if (infected[x] == 0) {
-                    infected[x] = 1;
-                    infectionTime[x] = t;
+            // 시간, 감염자 X, Y의 순서를 함께 교환
+            int temp = time[minIndex];
+            int temp1 = infectedX[minIndex];
+            int temp2 = infectedY[minIndex];
+            time[minIndex] = time[i];
+            infectedX[minIndex] = infectedX[i];
+            infectedY[minIndex] = infectedY[i];
+            time[i] = temp;
+            infectedX[i] = temp1;
+            infectedY[i] = temp2;
+        }
+
+        // 감염 확산 처리
+        int times = 0;
+        for (int i = 0; i < T; i++) {
+            if (i == 0 || time[i] - time[i - 1] < k) {
+                if(infected[infectedX[i]] == 1){
+                    infected[infectedY[i]] = 1;
+                    times++;
+                } else if (infected[infectedY[i]] == 0) {
+                    infected[infectedX[i]] = 1;
+                    times++;
                 }
+            }
+            if(times >= k){
+                break;
             }
         }
 
         // 결과 출력
-        for (int i = 1; i <= n; i++) {
+        for(int i = 1; i <= n; i++) {
             System.out.print(infected[i]);
         }
     }
