e015329a75420a372eaaa5e1516d024853bdde3e
margie1a <162654709+margie1a@users.noreply.github.com>
2024-08-24 16:31:55 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 156ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240824/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.java
+++ 240824/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.java
@@ -3,31 +3,31 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int k = sc.nextInt();
-        int p = sc.nextInt();
-        int T = sc.nextInt();
-        int[] infected = new int[n + 1]; // 이때 n+1은 n명을 처리하기 위한 배열
-        int[] time = new int[T];
-        int[] infectedX = new int[T];
-        int[] infectedY = new int[T];
+        int n = sc.nextInt(); // 개발자 수
+        int k = sc.nextInt(); // 감염 전파 가능한 최대 악수 횟수
+        int p = sc.nextInt(); // 최초 감염자
+        int T = sc.nextInt(); // 악수 이벤트 수
+
+        int[] infected = new int[n + 1]; // 각 개발자의 감염 여부
+        int[] handshakeCount = new int[n + 1]; // 각 개발자의 전염 가능 악수 횟수
+        int[] time = new int[T]; // 각 악수 이벤트의 시간
+        int[] infectedX = new int[T]; // 각 악수 이벤트에서의 x 개발자
+        int[] infectedY = new int[T]; // 각 악수 이벤트에서의 y 개발자
+
         infected[p] = 1; // 최초 감염자 설정
 
         // 입력받은 값을 배열에 저장
         for (int i = 0; i < T; i++) {
-            int t = sc.nextInt();
-            int x = sc.nextInt();
-            int y = sc.nextInt();
-            time[i] = t;
-            infectedX[i] = x;
-            infectedY[i] = y;
+            time[i] = sc.nextInt(); // 시간 t
+            infectedX[i] = sc.nextInt(); // x 개발자
+            infectedY[i] = sc.nextInt(); // y 개발자
         }
 
-        // 시간에 따른 이벤트 정렬
-        for(int i = 0; i < T; i++){
+        // 시간에 따른 이벤트 정렬 (기존 코드 유지)
+        for (int i = 0; i < T; i++) {
             int minIndex = i;
-            for(int j = i + 1; j < T; j++){
-                if(time[j] < time[minIndex]){
+            for (int j = i + 1; j < T; j++) {
+                if (time[j] < time[minIndex]) {
                     minIndex = j;
                 }
             }
@@ -44,22 +44,25 @@ public class Main {
         }
 
         // 감염 확산 처리
-        int times = 0;
         for (int i = 0; i < T; i++) {
-            if (i == 0 || time[i] - time[i - 1] < k) {
-                if(infected[infectedX[i]] == 1 || infected[infectedY[i]] == 1){
-                    infected[infectedX[i]] = 1;
-                    infected[infectedY[i]] = 1;
-                    times++;
-                }
+            int x = infectedX[i];
+            int y = infectedY[i];
+
+            // x가 감염되어 있고, 전염 가능 악수 횟수가 남아 있는 경우
+            if (infected[x] == 1 && handshakeCount[x] < k) {
+                infected[y] = 1; // y를 감염시킴
+                handshakeCount[x]++; // x의 전염 가능 악수 횟수 증가
             }
-            if(times >= k){
-                break;
+
+            // y가 감염되어 있고, 전염 가능 악수 횟수가 남아 있는 경우
+            if (infected[y] == 1 && handshakeCount[y] < k) {
+                infected[x] = 1; // x를 감염시킴
+                handshakeCount[y]++; // y의 전염 가능 악수 횟수 증가
             }
         }
 
         // 결과 출력
-        for(int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
             System.out.print(infected[i]);
         }
     }
