4a51fcc8e433ab0a67e4e9412d7c8243098f66e6
margie1a <162654709+margie1a@users.noreply.github.com>
2024-08-27 23:26:54 +0900

    Title: 격자 위의 편안한 상태, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240827/격자 위의 편안한 상태/comfortable-state-on-the-grid.java
+++ 240827/격자 위의 편안한 상태/comfortable-state-on-the-grid.java
@@ -2,20 +2,23 @@ import java.util.Scanner;
 
 public class Main {
     public static int n, m;
-    public static int r, c;
-    public static int[] dr = new int[]{0, 1, 0, -1}; // 우, 하, 좌, 상
-    public static int[] dc = new int[]{1, 0, -1, 0};
+    public static int[] dx = new int[]{1, 0, -1, 0}; // 동, 남, 서, 북
+    public static int[] dy = new int[]{0, -1, 0, 1};
+    public static int[][] map;
 
-    public static boolean isComfortable(int x, int y) {
+    public static boolean isRange(int x, int y) {
+        return (x >= 0 && x < n) && (y >= 0 && y < n);
+    }
+
+    public static boolean isComfortable(int a, int b){
         int cnt = 0;
-        for (int i = 0; i < 4; i++) {
-            int newR = x + dr[i];
-            int newC = y + dc[i];
-            if (newR >= 1 && newR <= n && newC >= 1 && newC <= n) {
+        for (int i = 0; i < n; i++) {
+            int newX = dx[i] + a, newY = dy[i] + b;
+            if(isRange(newX, newY) && map[newX][newY] == 1) {
                 cnt++;
             }
         }
-        if(cnt == 4){
+        if(cnt == 3) {
             return true;
         }
         return false;
@@ -25,10 +28,13 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
         m = sc.nextInt();
-        for(int i = 0; i < m; i++){
-            r = sc.nextInt();
-            c = sc.nextInt();
-            if(isComfortable(r,c)){
+        map = new int[n][n];
+
+        for (int i = 0; i < m; i++) {
+            int x = sc.nextInt() - 1;
+            int y = sc.nextInt() - 1;
+            map[x][y] = 1;
+            if(isComfortable(x, y)){
                 System.out.println(1);
             }else {
                 System.out.println(0);
