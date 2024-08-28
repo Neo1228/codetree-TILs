92c044ffb3f595ad95f3a20f0f4f383596b0dada
margie1a <162654709+margie1a@users.noreply.github.com>
2024-08-29 02:24:27 +0900

    Title: 거울에 레이저 쏘기 2, Time: 202ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240829/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.java
+++ 240829/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.java
@@ -2,24 +2,24 @@ import java.util.Scanner;
 
 public class Main {
     public static int n;
-    public static int razor;
-    public static int[] dx = new int[]{0, 1, 0, -1}; // 동, 남, 서, 북
-    public static int[] dy = new int[]{1, 0, -1, 0};
+    public static int[] dx = {0, 1, 0, -1}; // 동, 남, 서, 북
+    public static int[] dy = {1, 0, -1, 0}; // 동, 남, 서, 북
     public static char[][] map;
-    public static int[][] where;
 
-    public static boolean isRight(int x, int y) {
-        return (x >= 0 && x < n) && (y >= 0 && y < n);
+    // 격자 내에 있는지 확인하는 함수
+    public static boolean isWithinBounds(int x, int y) {
+        return x >= 0 && x < n && y >= 0 && y < n;
     }
 
-    public static int getReflection(int x, int y, int dir) {
+    // 레이저 반사 횟수를 계산하는 함수
+    public static int getReflectionCount(int x, int y, int dir) {
         int count = 0;
 
-        while(true){
+        while (true) {
             x += dx[dir];
             y += dy[dir];
 
-            if(!isRight(x,y)){
+            if (!isWithinBounds(x, y)) {
                 break;
             }
 
@@ -37,53 +37,35 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        where = new int[n][n];
         map = new char[n][n];
-        int x = 0, y = 0;
-        int dir = 0;
-        where[x][y] = 1;
-
-        for (int i = 2; i <= n * n; i++) {
-            int newX = x + dx[dir], newY = y + dy[dir];
-            if (newX == 0 && newY == 0) {
-                break;
-            }
-            if (!isRight(newX, newY) || where[newX][newY] != 0) {
-                dir = (dir + 1) % 4;
-            }
-            x += dx[dir];
-            y += dy[dir];
-            where[x][y] = i;
-        }
 
         for (int i = 0; i < n; i++) {
-            String str = sc.next();
-            for (int k = 0; k < n; k++) {
-                map[i][k] = str.charAt(k);
-            }
+            map[i] = sc.next().toCharArray();
         }
 
-        razor = sc.nextInt();
-        int startX = 0, startY = 0;
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                if(where[i][j] == razor) {
-                    startX = i;
-                    startY = j;
-                }
-            }
+        int razor = sc.nextInt();
+        int startX = 0, startY = 0, dir = 0;
+
+        // 레이저의 시작 위치 및 방향 설정
+        if (razor <= n) {
+            startX = 0;
+            startY = razor - 1;
+            dir = 1;  // 아래로
+        } else if (razor <= 2 * n) {
+            startX = razor - n - 1;
+            startY = n - 1;
+            dir = 2;  // 왼쪽으로
+        } else if (razor <= 3 * n) {
+            startX = n - 1;
+            startY = 3 * n - razor;
+            dir = 3;  // 위로
+        } else {
+            startX = 4 * n - razor;
+            startY = 0;
+            dir = 0;  // 오른쪽으로
         }
 
-        int direction = 0;
-        if (razor >= 1 && razor <= (4 * n) / 4) {
-            direction = 1;
-        } else if (razor > (4 * n) / 4 && razor <= (4 * n) / 4 * 2) {
-            direction = 2;
-        } else if (razor > (4 * n) / 4 * 2 && razor <= (4 * n) / 4 * 3) {
-            direction = 3;
-        } else if (razor > (4 * n) / 4 * 3 && razor <= (4 * n) / 4 * 4) {
-            direction = 0;
-        }
-        System.out.println(getReflection(startX, startY, direction));
+        int result = getReflectionCount(startX, startY, dir);
+        System.out.println(result);
     }
 }
