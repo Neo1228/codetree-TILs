6dbf2550bd279d3f902e352d3c9d4348acebf02f
margie1a <162654709+margie1a@users.noreply.github.com>
2024-08-29 10:48:47 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 251ms, Memory: 12MB, Status: Passed - CodeTree

--- 240829/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.java
+++ 240829/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.java
@@ -26,7 +26,7 @@ public class Main {
             y += dy[dir];
             map[x][y] = i;
         }
-        for(int i = 0; i < m; i++){
+        for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 System.out.print(map[i][j] + " ");
             }
