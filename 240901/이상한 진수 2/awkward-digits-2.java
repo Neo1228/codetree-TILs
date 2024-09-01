995c9b1a226a8ca960ad8ebd6f1ba988ae12c8bf
margie1a <162654709+margie1a@users.noreply.github.com>
2024-09-01 17:29:46 +0900

    Title: 이상한 진수 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240901/이상한 진수 2/awkward-digits-2.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int t = 0;
+        int count = 0;
+        int[] N = new int[100];
+        for (int i = 0; i < N.length; i++) {
+            t = a % 10;
+            a = a / 10;
+            N[i] = t;
+            count++;
+            if(a == 0){
+                break;
+            }
+        }
+
+        for(int i = count - 1; i >= 0; i--){
+            if(N[i] == 0){
+                N[i] = 1;
+                break;
+            }
+        }
+
+        int num = count - 1;
+        int sum = 0;
+        int cnt = -1;
+        while (true) {
+            cnt++;
+            if(N[num] == 1){
+                sum += (int) Math.pow(2, num);
+            }
+            num--;
+            if (num == 0) {
+                break;
+            }
+        }
+        System.out.println(sum);
+    }
+}
