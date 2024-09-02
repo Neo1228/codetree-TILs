6550a5433983a7a02b227658833d5f13390e3ac4
margie1a <162654709+margie1a@users.noreply.github.com>
2024-09-03 01:09:47 +0900

    Title: 원 모양으로 되어있는 방, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240903/원 모양으로 되어있는 방/a-room-in-a-circle.java
+++ 240903/원 모양으로 되어있는 방/a-room-in-a-circle.java
@@ -8,22 +8,21 @@ public class Main {
         for (int i = 0; i < n; i++){
             people[i] = sc.nextInt();
         }
+
         int min = Integer.MAX_VALUE;
-        int sum = 0;
+
         for (int i = 0; i < n; i++){
+            int sum = 0;
             for (int j = 0; j < n; j++) {
-                if(i == j){
-                    continue;
-                }
-                if(i > j){
+                if(i > j) {
                     sum += people[j] * (Math.abs(i - j) + 3);
-                }else {
+                } else {
                     sum += people[j] * Math.abs(i - j);
                 }
             }
             min = Math.min(min, sum);
-            sum = 0;
         }
+
         System.out.println(min);
     }
 }
