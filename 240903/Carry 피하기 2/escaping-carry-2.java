0ef043226ab5d9731c9f9d3ffde42d7c8e2bb1c4
margie1a <162654709+margie1a@users.noreply.github.com>
2024-09-03 11:39:47 +0900

    Title: Carry 피하기 2, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240903/Carry 피하기 2/escaping-carry-2.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static boolean isCarry(int a, int b, int c) {
+        int sum = a + b + c;
+        String strSum = Integer.toString(sum);
+        int len = strSum.length();
+        int n = 10;
+        int d = 0, f = 0, g = 0;
+        for (int i = 1; i <= len; i++) {
+            if(a > 0){
+                d = a % n;
+                a /= 10;
+            }
+            if(b > 0) {
+                f = b % n;
+                b /= 10;
+            }
+            if(c > 0) {
+                g = c % n;
+                c /= 10;
+            }
+            if(d + f + g >= 10) {
+                return false;
+            }
+            d = 0;
+            f = 0;
+            g = 0;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int sum = 0;
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if(isCarry(arr[i], arr[j], arr[k])) {
+                        max = Math.max(max, arr[i] + arr[j] + arr[k]);
+                    }
+                }
+            }
+        }
+        System.out.println(max);
+    }
+}
