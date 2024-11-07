f8252eea9fab85f3c4ceac5b9e066583d3c56bf2
최민수 <162654709+Neo1228@users.noreply.github.com>
2024-11-08 03:24:30 +0900

    Title: 선택 정렬 구현, Time: 89ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241108/선택 정렬 구현/implement-selection-sort.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < n; j++){
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            int tmp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = tmp;
+        }
+
+        for (int i = 0; i < n; i++) {
+            bw.write(String.valueOf(arr[i] + " "));
+        }
+        bw.flush();
+        bw.close();
+    }
+}
